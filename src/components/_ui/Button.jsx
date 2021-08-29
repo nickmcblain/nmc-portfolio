8a704f5f9530f55b429c6442027f91ba69c432c7
Nick McBlain <nickmcblain@gmail.com>
2021-08-29 15:22:11 +0100

    Init

---
+++ src/components/_ui/Button.jsx
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import styled from "@emotion/styled";
+import colors from "styles/colors";
+import dimensions from "styles/dimensions";
+
+const ButtonContainer = styled("button")`
+    padding: 1em 2em;
+    background: ${colors.grey900};
+    font-weight: 600;
+    color: white;
+    outline: none;
+    border: none;
+    font-size: 1rem;
+    border-radius: 5px;
+    position: relative;
+    transition: background 100ms ease-in-out;
+
+    @media(max-width:${dimensions.maxwidthMobile}px) {
+        padding: 0.8em 1.8em;
+        font-size: 1em;
+    }
+
+    p {
+        margin: 0;
+    }
+
+    &:before {
+        content: "";
+        position: absolute;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        /* background: linear-gradient(135deg, ${colors.grey700} 0%, ${colors.grey900} 100%); */
+        /* background: ${colors.grey800}; */
+        z-index: -1;
+    }
+
+    &:hover {
+        cursor: pointer;
+        background: ${colors.grey800};
+        transition: background 100ms ease-in-out;
+    }
+
+    &.Button--secondary {
+        background: ${colors.grey900};
+        color: white;
+        padding: 0.95em 1.8em;
+        font-size: 0.95rem;
+
+        &:hover {
+            background: ${colors.grey800};
+            transition: background 100ms ease-in-out;
+        }
+    }
+`
+
+class Button extends Component {
+    render() {
+        const { children, ...props } = this.props;
+        return (
+            <ButtonContainer
+                onClick={this.props.onClick}
+                {...props}>
+                {this.props.children}
+            </ButtonContainer>
+        );
+    }
+}
+
+export default Button;
---
+++ src/components/_ui/Logo.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Logo = () => (
+    <svg width="106px" height="25px" viewBox="0 0 106 25" version="1.1">
+        <title>nmc</title>
+        <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" font-family="Avenir-Black, Avenir" font-size="48" font-weight="700" letter-spacing="1.2">
+            <g id="Desktop-HD" transform="translate(-154.000000, -369.000000)" fill="#000000">
+                <text id="nmc.">
+                    <tspan x="152" y="393">nmc.</tspan>
+                </text>
+            </g>
+        </g>
+    </svg>
+);
+
+export default Logo;
